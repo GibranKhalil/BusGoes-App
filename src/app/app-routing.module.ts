fc75823db414279a82ddf15134399968cc9658af
Gibran Khalil <gibrankhalil1509@gmail.com>
2023-09-27 00:16:00 -0300

    Finalizando página inicio
    - inserção dos ideais
    - inserção de uma estrutura beta de rodapé

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { SobreDevsComponent } from './pages/sobre-devs/sobre-devs.component';
+import { RodapeComponent } from './content/rodape/rodape.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
+  {path: 'rodape', component: RodapeComponent},
   { path: 'inicio', component: InicioComponent },
   { path: 'sobre', component: SobreDevsComponent},
   {path: '', redirectTo: 'inicio', pathMatch: 'full'}
--- src/app/content/rodape/rodape.component.ts
+++ src/app/content/rodape/rodape.component.ts
@@ -7,4 +7,93 @@ import { Component } from '@angular/core';
 })
 export class RodapeComponent {
 
+  cidades = [
+    {
+      nome: 'Campinas'
+    },
+    {
+      nome: 'Sumaré'
+    },
+    {
+      nome: 'Hortolândia'
+    },
+    {
+      nome: 'São Paulo'
+    },
+    {
+      nome: 'Piracicaba'
+    },
+    {
+      nome: 'Guarulhos'
+    },
+    {
+      nome: 'Osasco'
+    },
+    {
+      nome: 'Sorocaba'
+    },
+    {
+      nome: 'Ribeirão Preto'
+    },
+    {
+      nome: 'Mogi das Cruzes'
+    },
+  ];
+
+  bilhetes = [
+    {
+      nome: 'Bilhete Comum'
+    },
+    {
+      nome: 'Bilhete Escolar'
+    },
+    {
+      nome: 'Bilhete Universitário'
+    },
+    {
+      nome: 'Bilhete Especial'
+    },
+    {
+      nome: 'Bilhete Idoso'
+    },
+    {
+      nome: 'Bilhete Mensal'
+    },
+    {
+      nome: 'Bilhete Anual'
+    },
+    {
+      nome: 'Bilhete Vale-Transporte'
+    },
+  ];
+
+  ajuda = [
+    {
+      nome: 'Fale com o suporte'
+    },
+    {
+      nome: 'Tutoriais'
+    },
+    {
+      nome: 'Denunciar Abuso'
+    }
+  ];
+
+  pays = [
+    {
+      img: 'assets/pix.png'
+    },
+    {
+      img: 'assets/visa.png'
+    },
+    {
+      img: 'assets/mastercard.png'
+    },
+    {
+      img: 'assets/elo.png'
+    },
+    {
+      img: 'assets/hipercard.png'
+    }
+  ]
 }
